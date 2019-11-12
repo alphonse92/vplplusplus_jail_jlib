d5b636721c1e96d33a3b6f88930c6707820e2cf6
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-11-12 13:10:03 -0500

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ echo "#!/bin/bash"  >> /usr/bin/vpl
 echo "export ENV=${ENV}" >>/usr/bin/vpl
 echo "export API_URL=${API_URL}" >>/usr/bin/vpl
 echo "export API_TOKEN=${API_TOKEN}" >>/usr/bin/vpl
+echo '. $1vpl_environment.sh'
 echo 'javac -cp "/usr/bin/VPLPlusPlus-jlib.jar" $1*.java' >> /usr/bin/vpl
 echo 'java -jar /usr/bin/VPLPlusPlus-jlib.jar -f $1' >> /usr/bin/vpl
-echo "${API_IP} ${API_HOSTNAME}" >>  /jail/etc/host
 chmod +x /usr/bin/vpl
 /etc/vpl/entrypoint.sh
