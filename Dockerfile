11177e1ff8bf3f987abbad065559460866803e84
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-11-11 22:06:45 -0500

    Adding to the prisoner the capability to reach the api

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM alphonse92/vpl-jail-execution-java:latest
 ARG JLIB_URL
 RUN cd /usr/bin/ \
   && curl $JLIB_URL >> VPLPlusPlus-jlib.jar \
-  && echo "#!/bin/bash" >> vpl \
-  && echo "java -jar /usr/bin/VPLPlusPlus-jlib.jar" >> vpl \
-  && chmod +x VPLPlusPlus-jlib.jar \
-  && chmod +x vpl 
-CMD ["/etc/vpl/entrypoint.sh"]
+  && chmod +x VPLPlusPlus-jlib.jar 
+COPY ./entrypoint.sh ./entrypoint.jlib.sh
+CMD ["/etc/vpl/entrypoint.jlib.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "#!/bin/bash"  >> /usr/bin/vpl
+echo "export ENV=${ENV}" >>/usr/bin/vpl
+echo "export API_URL=${API_URL}" >>/usr/bin/vpl
+echo "export API_TOKEN=${API_TOKEN}" >>/usr/bin/vpl
+echo 'javac -cp "/usr/bin/VPLPlusPlus-jlib.jar" $1*.java' >> /usr/bin/vpl
+echo 'java -jar /usr/bin/VPLPlusPlus-jlib.jar -f $1' >> /usr/bin/vpl
+echo "${API_IP} ${API_HOSTNAME}" >>  /jail/etc/host
+chmod +x /usr/bin/vpl
+/etc/vpl/entrypoint.sh
