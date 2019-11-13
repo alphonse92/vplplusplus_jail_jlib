af56ab859cc2ce52429d0989fa3771613d283af3
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-11-13 18:04:31 -0500

    asdasd

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ echo "#!/bin/bash"  >> /usr/bin/vpl
 echo "export ENV=${ENV}" >>/usr/bin/vpl
 echo "export API_URL=${API_URL}" >>/usr/bin/vpl
 echo "export API_TOKEN=${API_TOKEN}" >>/usr/bin/vpl
-echo '. $1vpl_environment.sh'
 echo 'javac -cp "/usr/bin/VPLPlusPlus-jlib.jar" $1*.java' >> /usr/bin/vpl
 echo 'java -jar /usr/bin/VPLPlusPlus-jlib.jar -f $1' >> /usr/bin/vpl
 chmod +x /usr/bin/vpl
