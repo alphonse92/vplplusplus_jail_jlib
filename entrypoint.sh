7eee8e1c739f33912bf65f32b6dcca99024d0598
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-11-13 20:11:24 -0500

    updating entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@ echo "#!/bin/bash"  >> /usr/bin/vpl
 echo "export ENV=${ENV}" >> /usr/bin/vpl
 echo "export API_URL=${API_URL}" >> /usr/bin/vpl
 echo "export API_TOKEN=${API_TOKEN}" >> /usr/bin/vpl
+echo 'echo "Testing class."'   >> /usr/bin/vpl
+echo 'echo ""' >> /usr/bin/vpl
 echo 'javac -cp "/usr/bin/VPLPlusPlus-jlib.jar" $1*.java' >> /usr/bin/vpl
 echo 'java -jar /usr/bin/VPLPlusPlus-jlib.jar -f $1' >> /usr/bin/vpl
 echo 'echo ""' >> /usr/bin/vpl
-echo 'echo "Run Script - Vpl ++ Jlib runner V.1.0.0"'  >> /usr/bin/vpl
+echo 'echo "Test Script - Vpl ++ Jlib runner V.1.0.0"'  >> /usr/bin/vpl
 echo 'echo "Ingeniería de Sistemas - UFPS"'  >> /usr/bin/vpl
 #Create script to run the test, adding to the claspath the jlib to get no errors
 echo "#!/bin/bash"  >> /usr/bin/vpl_run
