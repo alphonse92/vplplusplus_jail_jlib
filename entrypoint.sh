b6fae6ed2edfa4068ce282af8925f9ac850badfb
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2020-01-21 10:41:35 -0500

    improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+
+## Create script to test vpl++ activities
 rm -rf /usr/bin/vpl
 echo "#!/bin/bash"  >> /usr/bin/vpl
-echo "export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8"  >> /usr/bin/vpl
 echo "export ENV=${ENV}" >> /usr/bin/vpl
 echo "export API_URL=${API_URL}" >> /usr/bin/vpl
 echo "export API_TOKEN=${API_TOKEN}" >> /usr/bin/vpl
 echo 'echo "Testing class."'   >> /usr/bin/vpl
 echo 'echo ""' >> /usr/bin/vpl
-echo 'javac -cp "/usr/bin/VPLPlusPlus-jlib.jar" $1*.java' >> /usr/bin/vpl
+echo 'javac -nowarn -encoding UTF-8 -cp "/usr/bin/VPLPlusPlus-jlib.jar" $1*.java '  >> /usr/bin/vpl
 echo 'java -jar /usr/bin/VPLPlusPlus-jlib.jar -f $1' >> /usr/bin/vpl
 echo 'echo ""' >> /usr/bin/vpl
 echo 'echo "Test Script - Vpl ++ Jlib runner V.1.0.0"'  >> /usr/bin/vpl
 echo 'echo "Ingeniería de Sistemas - UFPS"'  >> /usr/bin/vpl
+
+
 #Create script to run the test, adding to the claspath the jlib to get no errors
 rm -rf /usr/bin/vpl_run
 echo "#!/bin/bash"  >> /usr/bin/vpl_run
-echo "export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8"  >> /usr/bin/vpl_run
 echo 'echo "Running class."'   >> /usr/bin/vpl_run
 echo 'echo ""'  >> /usr/bin/vpl_run
-echo 'javac -cp "/usr/bin/VPLPlusPlus-jlib.jar" $1*.java' >> /usr/bin/vpl_run
+echo 'javac -nowarn -encoding UTF-8  -cp "/usr/bin/VPLPlusPlus-jlib.jar" $1*.java' >> /usr/bin/vpl_run
 
 echo 'if test -f "main.class"; then' >> /usr/bin/vpl_run
 echo '    java -cp . main' >> /usr/bin/vpl_run
