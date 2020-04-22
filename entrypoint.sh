8d35ab9dd6de64ef41f24f28ae84e4ed753e4809
Eliecer Alejandro Molina Vergel <alejandromover92@gmail.com>
2020-04-22 11:05:29 -0500

    allow to modify the hosts files of process prissioners

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,25 @@ echo "#!/bin/bash " >> /usr/bin/vplplusplus_environment
 echo "export ENV=${ENV}" >> /usr/bin/vplplusplus_environment
 echo "export API_URL=${API_URL}" >> /usr/bin/vplplusplus_environment
 echo "export API_TOKEN=${API_TOKEN}" >> /usr/bin/vplplusplus_environment
+
+if [ -z "${CACERT}" ]; then
+    echo 'cacert file not found'
+else
+    if [ -f "${CACERT}" ]; then
+        echo "cacert file was found, setting cacert";
+        cp $CACERT /usr/local/share/ca-certificates
+        update-ca-certificates
+    fi;
+fi;
+
+if [ -z "${HOST_FILE}" ]; then
+    echo 'Host file not found'
+else
+    if [ -f "${HOST_FILE}" ]; then
+        echo "Host file found, adding to the prissioners process";
+        cat $HOST_FILE >> /jail/etc/hosts
+    fi;
+fi;
+
 ## Call the original entrypoint
 /etc/vpl/entrypoint.sh
