d6f925b565cc31492cf21753ac62b11b438ac2ee
Eliecer Alejandro Molina Vergel <alejandromover92@gmail.com>
2020-04-22 11:35:49 -0500

    organice the entrypoint to set the hosts prissioners file in temporal fs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-## Set the environment variables to the env var file name
+
+./vpl-jail-system stop >> /dev/null
+## Config the jail
+echo "PORT=${JAIL_PORT}" >> /etc/vpl/vpl-jail-system.conf
+echo "SECURE_PORT=${JAIL_SECURE_PORT}" >> /etc/vpl/vpl-jail-system.conf
+## Config the vpl plus plus jail
 echo "#!/bin/bash " >> /usr/bin/vplplusplus_environment
 echo "export ENV=${ENV}" >> /usr/bin/vplplusplus_environment
 echo "export API_URL=${API_URL}" >> /usr/bin/vplplusplus_environment
 echo "export API_TOKEN=${API_TOKEN}" >> /usr/bin/vplplusplus_environment
+
+## Install the jail in supervisor
+printf "[supervisord]\nnodaemon=false\n[program:vpl]\ncommand=/etc/vpl/vpl-jail-system start" >> /etc/supervisor/supervisord.conf  
+service supervisor start
+
+## Install certificate if exist
 
 if [ -z "${CACERT}" ]; then
     echo 'cacert file not found'
@@ -15,14 +26,20 @@ else
     fi;
 fi;
 
+## Install the custom hosts files for the process prissioners if exist
+
 if [ -z "${HOST_FILE}" ]; then
     echo 'Host file not found'
 else
     if [ -f "${HOST_FILE}" ]; then
         echo "Host file found, adding to the prissioners process";
         cat $HOST_FILE >> /jail/etc/hosts
+        cat /jail/etc/hosts
     fi;
 fi;
 
-## Call the original entrypoint
-/etc/vpl/entrypoint.sh
+## Tail the logs
+echo ""
+echo "Vpl Jail is listening on ${JAIL_PORT} and secure on  ${JAIL_SECURE_PORT}"
+echo ""
+tail -f /var/log/supervisor/supervisord.log
